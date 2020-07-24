d40094d4252364b788877345ee6945487c7a84a1
happyeven <1361840595@qq.com>
2020-07-24 22:21:20 +0800

    [TDD Parking Lot story 1] [Dong] feature:pass should_return_null_when_parking_boy_parking_car_given_car_and_parking_lot_has_no_position

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -3,14 +3,20 @@ package com.oocl.cultivation;
 import java.util.HashMap;
 
 public class ParkingLot {
-    HashMap<Ticket,Car> parkingCars = new HashMap<>();
+    int capacity = 10;
+    HashMap<Ticket, Car> parkingCars = new HashMap<>();
 
-    public Ticket parkingCarTOParkingLot(Car car){
+    public Ticket parkingCarTOParkingLot(Car car) {
+        if (capacity == 0) {
+            return null;
+        }
         Ticket ticket = new Ticket(car.getCarId());
-        this.parkingCars.put(ticket,car);
+        this.parkingCars.put(ticket, car);
+        capacity--;
         return ticket;
     }
-    public Car fetchingCarFromParkingLot(Ticket ticket){
+
+    public Car fetchingCarFromParkingLot(Ticket ticket) {
         return this.parkingCars.get(ticket);
     }
 }
