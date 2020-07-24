91030360e345f5c6181e0a3bd492673891848310
happyeven <1361840595@qq.com>
2020-07-24 20:51:27 +0800

    [TDD Parking Lot story 1] [Dong] refactor:override the equals() and hashCode() in class Car

--- src/main/java/com/oocl/cultivation/Car.java
+++ src/main/java/com/oocl/cultivation/Car.java
@@ -1,5 +1,7 @@
 package com.oocl.cultivation;
 
+import java.util.Objects;
+
 /**
  * @Author ZHUDO2
  * @Date 7/24/2020 3:58 PM
@@ -14,5 +16,18 @@ public class Car {
 
     public Car(int carId) {
         this.carId = carId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return carId == car.carId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carId);
     }
 }
