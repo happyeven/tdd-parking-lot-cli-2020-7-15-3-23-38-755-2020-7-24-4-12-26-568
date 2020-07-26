750a1be8e70625e4f91d0d85765e3554a84001bf
happyeven <1361840595@qq.com>
2020-07-26 18:32:58 +0800

    [TDD Parking Lot story 6] [Dong] refactor : ParkingLotServiceManager extends ParkingBoy and reduce the useless methods in ParkingLotServiceManager

--- src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
@@ -3,9 +3,8 @@ package com.oocl.cultivation;
 import java.util.LinkedList;
 import java.util.List;
 
-public class ParkingLotServiceManager {
+public class ParkingLotServiceManager extends ParkingBoy{
     private List<ParkingBoyStrategy> parkingBoys = new LinkedList<>();
-    private List<ParkingLot> parkingLotList = new LinkedList<>();
 
     public ParkingLotServiceManager(ParkingBoyStrategy... parkingBoyStrategies) {
         for (ParkingBoyStrategy parkingBoyStrategy : parkingBoyStrategies) {
@@ -33,29 +32,9 @@ public class ParkingLotServiceManager {
         return null;
     }
 
-    public Ticket parkingCar(Car car) {
-        if (car == null) {
-            return null;
+    public void setParkingLot(ParkingLot... parkingLotList) {
+        for (ParkingLot parkingLot : parkingLotList) {
+            this.parkingLots.add(parkingLot);
         }
-        if (this.parkingLotList.size() != 0) {
-            for (ParkingLot parkingLot : this.parkingLotList) {
-                if (parkingLot.getAvailableCapacity() != 0) {
-                    Ticket ticket = parkingLot.parkingCarTOParkingLot(car);
-                    ticket.setCorrespondParkingLot(parkingLot);
-                    return ticket;
-                }
-            }
-        }
-        return null;
-    }
-
-    public void setParkingLot(ParkingLot... parkingLots) {
-        for (ParkingLot parkingLot : parkingLots) {
-            this.parkingLotList.add(parkingLot);
-        }
-    }
-
-    public Car fetchingCar(Ticket ticket) {
-        return null;
     }
 }
